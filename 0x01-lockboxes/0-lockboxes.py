21a05ad1127118df41ab66dd3e9a030812a4fa8e
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-04-19 00:33:09 +0200

    alx-interview

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -9,6 +9,6 @@ def canUnlockAll(boxes):
     newKeys = [0]
     for newKey in newKeys:
         for key in boxes[newKey]:
-            if key not in newKeys and key <= len(boxes):
+            if key not in newKeys and key < len(boxes):
                 newKeys.append(key)
     return len(newKeys) == len(boxes)
