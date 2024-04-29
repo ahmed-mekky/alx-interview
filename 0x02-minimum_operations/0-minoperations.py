a34ad1a446d95f58da6f44b6e73fc96476324f6e
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-04-29 04:34:45 +0300

    minOperations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Calculates the fewest number of operations needed"""
+
+
+def minOperations(n):
+    """
+    Calculates the fewest number of operations needed
+    to result in exactly n H characters in the file.
+    """
+    main_primes = [2, 3, 5, 7, 9, 11]
+    for i in main_primes:
+        if n % i == 0:
+            if (n / i) in main_primes:
+                return i + int(n / i)
+            return i + minOperations(int(n / i))
+    return n
