69b5795fb19617b35d3b2c0394e31f8eaee11fc6
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-05-03 04:37:49 +0300

    log_parsing

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import sys
+
+codes_dict = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
+file_size = 0
+counter = 0
+try:
+    for line in sys.stdin:
+        file_size += int(line.split()[-1])
+        code = line.split()[-2]
+        counter += 1
+        if code in codes_dict:
+            codes_dict[code] += 1
+            if (counter % 10 == 0 and counter != 0):
+                print(f'File size: {file_size}')
+                for code in codes_dict.keys():
+                    print(f'{code}: {codes_dict[code]}')
+except KeyboardInterrupt:
+    print(f'File size: {file_size}')
+    for code in codes_dict.keys():
+        print(f'{code}: {codes_dict[code]}')
