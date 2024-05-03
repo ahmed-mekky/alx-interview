48db1a72c5663b65be4297855ff59255f308832b
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-05-03 04:40:43 +0300

    log_parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
 import sys
-
-codes_dict = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
-file_size = 0
+"""
+This script reads from standard input and computes metrics
+"""
+codes_dict = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0}
 counter = 0
 try:
     for line in sys.stdin:
