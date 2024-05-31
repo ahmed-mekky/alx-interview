241bb99db6cea21da768a07009aa465778a4fd41
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-05-31 05:57:41 +0300

    matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """Rotate 2D Matrix"""
-import copy
 
 
 def rotate_2d_matrix(matrix):
     """rotates the matrix"""
     length = len(matrix)
-    new_matrix = copy.deepcopy(matrix)
+    new_matrix = []
+    for i in range(length):
+        new_matrix.append([])
+        for j in range(length):
+            new_matrix[i].append(matrix[i][j])
     for i in range(length):
         for j in range(length):
             matrix[j][length - 1 - i] = new_matrix[i][j]
