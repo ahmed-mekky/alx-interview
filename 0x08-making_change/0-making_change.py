9eda460208eb38eaa35a723c35bc1b32d78f145f
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-06-07 05:49:36 +0300

    Change comes from within

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""making change (coins) module"""
+
+
+def makeChange(coins, total):
+    """main function"""
+    if total <= 0:
+        return 0
+    coins = sorted(coins, reverse=True)
+    coin = coins[0]
+    n = 0
+    while True:
+        if coin > total:
+            if coin == 0 or total == 0:
+                return n
+            elif coin is coins[-1]:
+                return -1
+            coin = coins[coins.index(coin) + 1]
+            continue
+        n += 1
+        total -= coin
