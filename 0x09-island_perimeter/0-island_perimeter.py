558a54e8ba7bb04c10c94d59e04397ee65a939c3
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-06-16 20:30:47 +0300

    island_perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -20,5 +20,5 @@ def island_perimeter(grid):
                     if not grid[i][j - 1]:
                         sum += 1
                 except IndexError:
-                    return None
+                    return 0
     return sum
