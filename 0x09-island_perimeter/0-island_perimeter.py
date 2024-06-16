12d3b33cf0d4e50d2004641a6c1f50c1562f1fcc
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-06-16 21:02:46 +0300

    island_perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -4,20 +4,25 @@
 
 def island_perimeter(grid):
     """main func"""
-    length = len(grid)
     sum = 0
-    for i in range(length):
-        for j in range(length):
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
             item = grid[i][j]
             if item:
                 try:
-                    if not grid[i - 1][j]:
+                    if i > 0:
+                        if not grid[i - 1][j]:
+                            sum += 1
+                    else:
+                        sum += 1
+                    if j > 0:
+                        if not grid[i][j - 1]:
+                            sum += 1
+                    else:
                         sum += 1
                     if not grid[i + 1][j]:
                         sum += 1
                     if not grid[i][j + 1]:
-                        sum += 1
-                    if not grid[i][j - 1]:
                         sum += 1
                 except IndexError:
                     sum += 1
